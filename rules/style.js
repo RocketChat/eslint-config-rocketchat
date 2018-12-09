aed49168cf6927525441c064a9421a2ce3bfbab9
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-12-08 23:58:36 -0200

    Require space between `async` and arrow function

--- rules/style.js
+++ rules/style.js
@@ -421,7 +421,7 @@ module.exports = {
 
     // require or disallow space before function opening parenthesis
     // https://eslint.org/docs/rules/space-before-function-paren
-    'space-before-function-paren': ['error', 'never'],
+    'space-before-function-paren': ['error', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
 
     // require or disallow spaces inside parentheses
     'space-in-parens': ['error', 'never'],
